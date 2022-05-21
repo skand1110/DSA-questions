2dfb788d0b89ff36a226b95bddeec008949c5dd2
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-05-21 21:46:56 +0530

    Update 37_spiral_traversal.cpp

--- matrix/37_spiral_traversal.cpp
+++ matrix/37_spiral_traversal.cpp
@@ -1,1 +1,55 @@
+//Approach 1: Simple traversal under the limits using four pointers
+//TC: O(r*c); SC: O(1)
 
+// C++ Program to print a matrix spirally
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define R 4
+#define C 4
+ 
+void spiralTraversal(int m, int n, int a[R][C])
+{
+    int i, k = 0, l = 0;
+ 
+    /* k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator
+    */
+ 
+    while (k < m && l < n) {
+        /* Print the first row from
+               the remaining rows */
+        for (i = l; i < n; ++i) {
+            cout << a[k][i] << " ";
+        }
+        k++;
+ 
+        /* Print the last column
+         from the remaining columns */
+        for (i = k; i < m; ++i) {
+            cout << a[i][n - 1] << " ";
+        }
+        n--;
+ 
+        /* Print the last row from
+                the remaining rows */
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << a[m - 1][i] << " ";
+            }
+            m--;
+        }
+ 
+        /* Print the first column from
+                   the remaining columns */
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
