bcb988e25731df6c41a63c4b3dbff3b09087da12
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-05-28 18:03:30 +0530

    Add files via upload

---
+++ searching and sorting/93_square_root_of_an_int.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int countSquares(int N) {
+        int count = 0;
+        for(int i = 1; i*i<N; i++){
+                count++;
+        }
+        return count;
+    }
+};
