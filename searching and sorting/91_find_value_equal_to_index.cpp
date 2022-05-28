50552c9120491a6fb924fa03b7ed3a95298c73ba
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-05-28 17:16:31 +0530

    Add files via upload

---
+++ searching and sorting/91_find_value_equal_to_index.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.c>
+using namespace std;
+
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    vector<int> v;
+	    for(int i=0; i<n; i++){
+	        if(arr[i]==i+1){
+	            v.push_back(i+1);
+	        }
+	    }
+	    return v;
+	}
+};
