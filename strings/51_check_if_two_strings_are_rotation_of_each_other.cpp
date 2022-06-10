f945fe40b4b520706008d5a74bf3c57d37b7b788
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-06-10 14:30:26 +0530

    Update 51_check_if_two_strings_are_rotation_of_each_other.cpp

--- strings/51_check_if_two_strings_are_rotation_of_each_other.cpp
+++ strings/51_check_if_two_strings_are_rotation_of_each_other.cpp
@@ -29,8 +29,11 @@ bool areRotations(string str1, string str2)
         return false;
  
    string temp = str1 + str1;
-  return (temp.find(str2) != string::npos); 
-}
+  return (temp.find(str2) != string::npos); //It is a constant static member value with the highest possible value for an element of type size_t.
+}                                           /* It actually means until the end of the string.
+                                               It is used as the value for a length parameter in the string’s member functions.
+                                               As a return value, it is usually used to indicate no matches. */
+
 
 //-------------------------------------------------------
 //Approach 2: using STL and queue
