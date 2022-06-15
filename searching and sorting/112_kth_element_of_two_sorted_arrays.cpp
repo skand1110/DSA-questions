ddfd5c23e045546af261663acae37bebb3ad888f
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-06-15 18:46:03 +0530

    Create 112_kth_element_of_two_sorted_arrays.cpp

---
+++ searching and sorting/112_kth_element_of_two_sorted_arrays.cpp
@@ -0,0 +1,31 @@
+//Given two sorted arrays of size m and n respectively, you are tasked with finding the element that would be at the k’th position of the final sorted array.
+//Approach:  use 2 pointers to find the k-th element. Or use binary search.
+/*
+Since we are given two sorted arrays, we can use the merging technique to get the final merged array. However, this solution will have a time complexity of O(n+m) and 
+so will be the space complexity. We can optimise this brute method by using divide and conquer approach of pointers or binary search. This will have time complexity
+of O(logk)
+*/
+int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if (m < n)
+            return kthElement(arr2, arr1, m, n, k);
+
+        int low = max(0, k - m), high = min(k, n);
+
+        while (low <= high)
+        {
+            int cut1 = (low + high) >> 1;
+            int cut2 = k - cut1;
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1];
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1];
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2];
+
+            if (l1 <= r2 && l2 <= r1)
+                return max(l1, l2);
+            else if (l1 > r2)
+                high = cut1 - 1;
+            else
+                low = cut1 + 1;
+        }
+        return 0;
