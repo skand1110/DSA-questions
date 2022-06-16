4de889b4ae0d64efdcfc5a11aff473ccb30054cb
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-06-16 16:46:58 +0530

    Create 114_book_allocation.cpp

---
+++ searching and sorting/114_book_allocation.cpp
@@ -0,0 +1,52 @@
+//binary search
+
+bool isValid(int a[],int n, int m,int mid){
+        int sum=0;
+        int student=1;
+        for(int i=0;i<n;i++){
+            sum = sum+a[i];
+            if(sum>mid){
+                student++;
+                sum = a[i];
+            }
+            if(student>m){
+                return false;
+            }
+        }
+        return true;
+}
+class Solution 
+{
+    public:
+    //Function to find minimum number of pages.
+    int findPages(int a[], int n, int m) 
+    {
+        //code here
+        int max=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        int start=max;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum += a[i];
+        }
+        int end=sum;
+        int ans=-1;
+        int mid;
+        while(start<=end){
+            mid= start + (end-start)/2;
+            if(isValid(a,n,m,mid)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
