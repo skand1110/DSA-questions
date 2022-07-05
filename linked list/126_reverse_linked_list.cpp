9b23ae6df068f1c34881f35cc2fef802d3a63e1e
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-07-05 18:55:38 +0530

    Update 126_reverse_linked_list.cpp

--- linked list/126_reverse_linked_list.cpp
+++ linked list/126_reverse_linked_list.cpp
@@ -1,1 +1,140 @@
+//Approach 1: iterative
+//time complexity: O(n); space complexity: O(1)
+/*
+Initialize three pointers prev as NULL, curr as head and next as NULL.
+Iterate through the linked list. In loop, do following. 
+// Before changing next of current, 
+// store next node 
+next = curr->next
+// Now change next of current 
+// This is where actual reversing happens 
+curr->next = prev 
+// Move prev and curr one step forward 
+prev = curr 
+curr = next
+*/
 
+// Iterative C++ program to reverse a linked list
+#include <iostream>
+using namespace std;
+ 
+/* Link list node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+ 
+struct LinkedList {
+    Node* head;
+    LinkedList() { head = NULL; }
+ 
+    /* Function to reverse the linked list */
+    void reverse()
+    {
+        // Initialize current, previous and next pointers
+        Node* current = head;
+        Node *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+            // Reverse current node's pointer
+            current->next = prev;
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+ 
+    /* Function to print linked list */
+    void print()
+    {
+        struct Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+ 
+    void push(int data)
+    {
+        Node* temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+};
+
+//----------------------------------------------------------------
+//Approach 2: REcursive
+//time complexity: O(); space complexity: O()
+/*
+ 1) Divide the list in two parts - first node and 
+      rest of the linked list.
+   2) Call reverse for the rest of the linked list.
+   3) Link rest to first.
+   4) Fix head pointer
+*/
+
+// Recursive C++ program to reverse
+// a linked list
+#include <iostream>
+using namespace std;
+ 
+/* Link list node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+ 
+struct LinkedList {
+    Node* head;
+    LinkedList()
+    {
+        head = NULL;
+    }
+ 
+    Node* reverse(Node* head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+ 
+        /* reverse the rest list and put
+          the first element at the end */
+        Node* rest = reverse(head->next);
+        head->next->next = head;
+ 
+        /* tricky step -- see the diagram */
+        head->next = NULL;
+ 
+        /* fix the head pointer */
+        return rest;
+    }
+ 
+    /* Function to print linked list */
+    void print()
+    {
+        struct Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+ 
+    void push(int data)
+    {
+        Node* temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+};
