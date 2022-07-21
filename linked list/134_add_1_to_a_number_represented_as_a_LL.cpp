36dbb34308b39d2549684d2c564142cd17c2e106
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-07-21 20:02:07 +0530

    Create 134_add_1_to_a_number_represented_as_a_LL.cpp

---
+++ linked list/134_add_1_to_a_number_represented_as_a_LL.cpp
@@ -0,0 +1,7 @@
+//approach: using reverse linked list
+/*
+Reverse given linked list. For example, 1-> 9-> 9 -> 9 is converted to 9-> 9 -> 9 ->1.
+Start traversing linked list from leftmost node and add 1 to it. If there is a carry, move to the next node. Keep moving to the next node while there is a carry.
+Reverse modified linked list and return head.
+*/
+
